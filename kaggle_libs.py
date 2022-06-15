e9760f4c517244de82413e3d4ec17a5e1aaed5ca
Diem Nguyen <dnguyen@twitter.com>
2022-06-14 17:10:45 -0700

    Update library setup

--- kaggle_libs.py
+++ kaggle_libs.py
@@ -1,18 +1,19 @@
-def setup_notebook():
-	import pandas as pd
-	import numpy as np
-	import matplotlib.pylab as plt
-	color_pal = plt.rcParams['axes.prop_cycle'].by_key()['color']
+# setup libraries
 
-	import warnings
-	warnings.filterwarnings("ignore")
-	plt.style.use('ggplot')
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+color_pal = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+import warnings
+warnings.filterwarnings("ignore")
+plt.style.use('ggplot')
 
 
-def read_kaggle_data():
-	train = pd.read_csv("../input/train.csv")
-	test = pd.read_csv("../input/test.csv")
-	ss = pd.read_csv("../input/sample_submission.csv")
+def read_kaggle_data(input_path="../input"):
+	train = pd.read_csv(f"{input_path}/train.csv")
+	test = pd.read_csv(f"{input_path}/test.csv")
+	ss = pd.read_csv(f"{input_path}/sample_submission.csv")
 
 	train["isTrain"] = True
 	test["isTrain"] = False
