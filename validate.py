85be869d8e979c06df9deec88322e358540bb895
Diem Nguyen <dnguyen@twitter.com>
2022-06-15 18:51:56 -0700

    Add validate

---
+++ validate.py
@@ -0,0 +1,15 @@
+def add_kfold(data):
+
+    from sklearn import model_selection
+
+    data.train["kfold"] = -1
+
+    kf = model_selection.StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+    # TODO: stratify by both label and file_extension
+    for f, (t_, v_) in enumerate(kf.split(X=data.train, y=data.train.label)):
+        data.train.loc[v_, 'kfold'] = f
+
+    print(data.train.sample(3))
+
+    data.train.to_csv("../input/train_folds.csv", index=False)
